3a2b7d98ddf80d38a62c3143d2ae99d8fed9dc6c
Maciej Rutkowski <maciej.rutkowsky@gmail.com>
2024-01-28 20:13:58 +0100

    testing second function

--- function_app.py
+++ function_app.py
@@ -23,3 +23,24 @@ def test_azure_function(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+@app.route(route="second-test-azure-function")
+def second_test_azure_function(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This second HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
